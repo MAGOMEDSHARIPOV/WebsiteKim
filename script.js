5c59e680d92801b955596ab5645dfbbee2277503
MAGOMEDSHARIPOV <161922870+MAGOMEDSHARIPOV@users.noreply.github.com>
2025-05-19 20:26:56 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-function showMessage() {
-    alert("Hello! This is a message from JavaScript.");
-}
+document.getElementById("contact-form").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Thank you, Jesver will get back to you soon!");
+});
+
 
